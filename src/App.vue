1b93e6f37a4127d0e2a4da6df877957c1b9cb35c
abdellatif rhounan <abdellatif.rhounan@gmail.com>
2023-12-08 20:51:43 +0000

    Syntax Verified

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,24 @@
   <section class="greeting">
     <h2 class="heading">
       What's up,
-      <input type="text" class="myname" placeholder="My Name" v-model="myName" maxlength="20" />
+      <input
+        type="text"
+        class="myname"
+        placeholder="Your Name"
+        maxlength="20"
+        v-model="myName"
+      />
     </h2>
   </section>
 
   <section class="newtodo">
     <form class="todo-form" @submit.prevent="addTodo">
-      <input type="text" class="todo-input" placeholder="Add Something" v-model="newTodo" />
+      <input
+        type="text"
+        class="todo-input"
+        placeholder="Add Something"
+        v-model="newTodo"
+      />
 
       <button type="submit" class="todo-submit">
         <i class="bx bx-plus"></i>
@@ -19,56 +30,102 @@
   <section class="todo-list">
     <h2 class="heading">Todo List</h2>
 
-    <div class="todos-footer">
-      <div class="footer-item">
+    <div class="todos-control">
+      <div class="c-row">
         <div class="checkall">
-          <input type="checkbox" class="checkbox" id="checkAll" :checked="noRemaining" @change="checkAll" />
+          <input
+            type="checkbox"
+            class="checkbox"
+            id="checkAll"
+            :checked="allDone"
+            @change="checkAll"
+          />
           <label class="label" for="checkAll">Check All</label>
         </div>
 
-        <div class="remaining">{{ remaining }} items left</div>
+        <div class="remaining">
+          {{ remaining }} item{{ remaining > 1 ? "s" : "" }} left
+        </div>
       </div>
 
-      <div class="footer-item">
+      <div class="c-row">
         <div class="filters">
-          <button :class="{ active: filter == 'all' }" @click="filter = 'all'">All</button>
-          <button :class="{ active: filter == 'active' }" @click="filter = 'active'">Active</button>
-          <button :class="{ active: filter == 'completed' }" @click="filter = 'completed'">Completed</button>
+          <button
+            :class="{ active: myfilter == 'all' }"
+            @click="myfilter = 'all'"
+          >
+            All
+          </button>
+          <button
+            :class="{ active: myfilter == 'active' }"
+            @click="myfilter = 'active'"
+          >
+            Active
+          </button>
+          <button
+            :class="{ active: myfilter == 'completed' }"
+            @click="myfilter = 'completed'"
+          >
+            Completed
+          </button>
         </div>
 
-        <button class="clear-completed" v-show="showClearCompleted" @click="clearCompleted">
+        <button
+          class="clear-completed"
+          v-show="showClearCompleted"
+          @click="clearCompleted"
+        >
           Clear Completed
         </button>
       </div>
     </div>
 
     <div v-if="anyTodos" class="content">
-      <div class="todo-item" v-for="todo in todos_asc" :key="todo.createdAt" :class="{ completed: todo.completed }">
-
+      <div
+        class="todo-item"
+        v-for="todo in filterdTodos"
+        :key="todo.id"
+        :class="{ completed: todo.completed }"
+      >
         <div class="todo-check">
-          <input type="checkbox" class="checkbox" :id="`checkbox-${todo.createdAt}`" v-model="todo.completed" />
-          <label class="checkspan" :for="`checkbox-${todo.createdAt}`"></label>
+          <input
+            type="checkbox"
+            class="checkbox"
+            :id="`checkbox-${todo.id}`"
+            v-model="todo.completed"
+          />
+          <label class="checkspan" :for="`checkbox-${todo.id}`"></label>
         </div>
 
-        <input type="text" class="todo-text" :class="{ editing: todo.editing }" :readonly="!todo.editing"
-          v-model="todo.text" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" />
+        <input
+          type="text"
+          class="todo-text"
+          :class="{ editing: todo.editing }"
+          :readonly="!todo.editing"
+          v-model="todo.text"
+          @blur="doneEdit(todo)"
+          @keyup.enter="doneEdit(todo)"
+          @keyup.esc="cancelEdit(todo)"
+        />
 
         <div class="actions">
-          <i class="bx bxs-edit-alt edit-item" v-show="!todo.editing" @click.left="editTodo($event, todo)">
+          <i
+            v-if="!todo.editing"
+            class="bx bxs-edit-alt edit-item"
+            @click="editTodo($event, todo)"
+          >
           </i>
+          <i v-else class="bx bxs-save save-item"></i>
 
-          <i class="bx bxs-save save-item" v-show="todo.editing"></i>
-
-          <i class="bx bxs-trash-alt remove-item" @click.left="removeTodo(todo)"></i>
+          <i class="bx bxs-trash-alt remove-item" @click="removeTodo(todo)"></i>
         </div>
-
       </div>
     </div>
     <div v-else class="no-task">No Task!</div>
   </section>
 
   <footer class="footer">
-    Copyright &copy; {{ new Date().getFullYear() }} |
+    Copyright &copy; {{ yearNow }} |
     <span>&nbsp;Abdellatif RHOUNAN</span>
   </footer>
 </template>
@@ -76,47 +133,48 @@
 <script>
 export default {
   name: "App",
+
   data() {
     return {
-      myName: "",
+      myName: null,
       newTodo: "",
       todos: [],
-      beforeEdit: "",
-      filter: "all",
+      idForTodo: null,
+      preEdit: "",
+      myfilter: "all",
     };
   },
 
   computed: {
-    todos_asc() {
-      return [...this.todos_filterd].sort((a, b) => a.createdAt - b.createdAt);
-    },
-
-    todos_filterd() {
-      if (this.filter == 'all') {
+    filterdTodos() {
+      if (this.myfilter == "all") {
         return this.todos;
+      } else if (this.myfilter == "active") {
+        return this.todos.filter((e) => !e.completed);
+      } else if (this.myfilter == "completed") {
+        return this.todos.filter((e) => e.completed);
       }
-      else if (this.filter == 'active') {
-        return this.todos.filter(e => !e.completed);
-      }
-      else if (this.filter == 'completed') {
-        return this.todos.filter(e => e.completed);
-      }
+      return this.todos;
     },
 
     remaining() {
-      return this.todos.filter(e => !e.completed).length;
+      return this.todos.filter((e) => !e.completed).length;
     },
 
-    noRemaining() {
-      return (this.remaining == 0);
+    allDone() {
+      return this.remaining == 0;
     },
 
     showClearCompleted() {
-      return (this.todos.filter(e => e.completed).length > 0);
+      return this.todos.filter((e) => e.completed).length > 0;
     },
 
     anyTodos() {
-      return (this.todos_asc.length > 0);
+      return this.filterdTodos.length > 0;
+    },
+
+    yearNow() {
+      return new Date().getFullYear();
     },
   },
 
@@ -127,12 +185,14 @@ export default {
       }
 
       this.todos.push({
+        id: this.idForTodo,
         text: this.newTodo.trim(),
         completed: false,
         editing: false,
-        createdAt: new Date().getTime(),
       });
+
       this.newTodo = "";
+      this.idForTodo++;
     },
 
     removeTodo(todo) {
@@ -140,20 +200,20 @@ export default {
     },
 
     editTodo(event, todo) {
-      this.beforeEdit = todo.text;
+      this.preEdit = todo.text;
       todo.editing = true;
       event.srcElement.parentElement.previousSibling.focus();
     },
 
     doneEdit(todo) {
       if (todo.text.trim().length == 0) {
-        todo.text = this.beforeEdit;
+        todo.text = this.preEdit;
       }
       todo.editing = false;
     },
 
     cancelEdit(todo) {
-      todo.text = this.beforeEdit;
+      todo.text = this.preEdit;
       todo.editing = false;
     },
 
@@ -164,7 +224,7 @@ export default {
     },
 
     clearCompleted() {
-      this.todos = this.todos.filter(e => !e.completed);
+      this.todos = this.todos.filter((e) => !e.completed);
     },
   },
 
@@ -172,17 +232,23 @@ export default {
     myName(newVal) {
       localStorage.setItem("myName", newVal);
     },
+
     todos: {
       handler(newVal) {
         localStorage.setItem("todos", JSON.stringify(newVal));
       },
       deep: true,
     },
+
+    idForTodo(newVal) {
+      localStorage.setItem("idForTodo", newVal);
+    },
   },
 
   mounted() {
     this.myName = localStorage.getItem("myName") || "";
     this.todos = JSON.parse(localStorage.getItem("todos")) || [];
+    this.idForTodo = +localStorage.getItem("idForTodo") || 0;
   },
 };
 </script>
